5e98153b0f3a26be94f2ac06e93c4ab1269f36cd
Christoph551 <christophersimmonds551@gmail.com>
2023-01-05 15:54:42 -0700

    Saving changes

--- Assets/script.js
+++ Assets/script.js
@@ -1,12 +1,20 @@
 // Need to have a function display "Correct" in green font or "Incorrect in red font based off of the user's input."
-const right = 0;
-const wrong = 1;
-let answer = [];
+// let timer = $('.Timer');
+// let currentScore = $('.Current-Score');
+// let main = $('.Container');
+// let questionNumber = $('Question-Number');
+
+let currentScore = function () {
+    document.getElementById("#Current-Score").innerHTML = "Current Score: ";
+    document.getElementById("#Current-Score").style.color = "white";
+}
+
 
 let correct = function () {
     document.getElementById("Correct-Answer-Feedback").innerHTML = "Correct!";
     document.getElementById("Correct-Answer-Feedback").style.color = "green";
     console.log("Correct!");
+    return;
 } 
 
 let incorrect = function () {
@@ -15,9 +23,13 @@ let incorrect = function () {
     console.log("Incorrect!");
 } 
 
-
-
 document.getElementById("Button1").addEventListener("click", incorrect);
 document.getElementById("Button2").addEventListener("click", incorrect);
 document.getElementById("Button3").addEventListener("click", incorrect);
 document.getElementById("Button4").addEventListener("click", correct);
+
+document.getElementById("startBtn").addEventListener("click", function() {
+    console.log("start");
+    document.getElementById("startUpDisplay").style.display = "block";
+});
+
