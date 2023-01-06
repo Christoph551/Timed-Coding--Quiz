733bb39df29206093f8ed6b0cb0b415c34758ba2
Christoph551 <christophersimmonds551@gmail.com>
2023-01-05 21:48:19 -0700

    Saving changes for now. Having trouble with timer function. Going to try starting from scratch in a fresh repo.

--- Assets/script.js
+++ Assets/script.js
@@ -4,33 +4,47 @@ const button3 = 3;
 const button4 = 4;
 let startTime;
 
+
+let highScores = function () {
+    
+}
+
+
+
+
+
+
+// document.getElementById("View-High-Scores").addEventListener("click", highScores) 
+// document.getElementById("View-High-Scores").style.display = "block";
+
+
 // Function to add an event listener to the Start Button.  
 // Initially have <main> displayed to none. Then this function will display <main> which contains the questions and answer options.
 // This function will then display the current timer and count down from 60.
 document.getElementById("startBtn").addEventListener("click", function() {
     console.log("start");
     document.getElementById("startUpDisplay").style.display = "block";
-    currentTime = 30;
+    let currentTime = 30;
     let timeLeft = setInterval(timer, 1000);
         function timer(){
-            currentTime = currentTime - 1;
-            if (count <= 0) { // This does not appear to be working at the moment. Console log firing back a syntax error
-                clearInterval(currentTime);
-                return;
-            } 
-    }
-    let startTime = currentTime + " Seconds on the clock!";
-    if (timeLeft = startTime) {
-    document.getElementById("Current-Score").innerHTML = startTime;
-    document.getElementById("startBtn").style.display = "none";
-        } else if (currentTime === 0) {
-        window.alert("You have run out of time. Press `OK` to save your score");
+            (currentTime = currentTime - 1);
         }
-    return;
+        if (timeLeft <= currentTime) {
+            document.getElementById("Current-Score").innerHTML = currentTime + " Seconds on the clock!"
+            document.getElementById("startBtn").style.display = "none";
+        } else if (currentTime === 0) {
+            window.alert("You have run out of time. Press `OK` to save your score");
+        }
+        if (count = 0) { // This does not appear to be working at the moment. Console log firing back a syntax error
+            clearInterval(currentTime);
+        }
     }
 );
 
-// Correct answer function to diplay text and stylize that added text.
+
+
+
+            // Correct answer function to diplay text and stylize that added text.
 // Will need to call this function when building the algorithm to randomize questions.
 let correct = function () {
     document.getElementById("Correct-Answer-Feedback").innerHTML = "Correct!";
